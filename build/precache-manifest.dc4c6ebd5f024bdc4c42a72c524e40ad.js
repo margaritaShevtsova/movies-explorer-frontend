5e88ed45af1899810fbdb57d5a7b96d3288845b2
Margarita Shevtsova <molegovn@mail.ru>
2023-08-22 15:17:13 +0300

    React функциональность

---
+++ build/precache-manifest.dc4c6ebd5f024bdc4c42a72c524e40ad.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "8600b8be2c5f81789ac6",
+    "url": "/static/css/main.03baa2ac.chunk.css"
+  },
+  {
+    "revision": "8600b8be2c5f81789ac6",
+    "url": "/static/js/main.8600b8be.chunk.js"
+  },
+  {
+    "revision": "67a8a2d6ecc0696c35d3",
+    "url": "/static/js/1.67a8a2d6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f43312f87d8d8f0a025fa346dd8aa300",
+    "url": "/static/media/logo_main-1.f43312f8.svg"
+  },
+  {
+    "revision": "c04e655916ff7ee03b404e13b2ed9e8a",
+    "url": "/static/media/arrow.c04e6559.svg"
+  },
+  {
+    "revision": "1ec56c8ddae4c4f3e7e19ede373f3d23",
+    "url": "/static/media/student.1ec56c8d.png"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/static/media/Success.e38b395c.svg"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "/static/media/Fale.4072126c.svg"
+  },
+  {
+    "revision": "dc131113894217b5031000575d9de002",
+    "url": "/static/media/Inter-Regular.dc131113.woff2"
+  },
+  {
+    "revision": "75db5319e7e87c587019a5df08d7272c",
+    "url": "/static/media/Inter-Medium.75db5319.woff2"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "/static/media/Inter-Regular.3ac83020.woff"
+  },
+  {
+    "revision": "661569afe57a38e1529a775a465da20b",
+    "url": "/static/media/Inter-Black.661569af.woff2"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "/static/media/Inter-Black.d0b121f3.woff"
+  },
+  {
+    "revision": "c0638bea87a05fdfa2bb3bba2efe54e4",
+    "url": "/static/media/Inter-Medium.c0638bea.woff"
+  },
+  {
+    "revision": "66af49748881ea0c4e9d07d5c588f532",
+    "url": "/static/media/close-btn.66af4974.svg"
+  },
+  {
+    "revision": "3166ed04ade02aba6333b23c03a1df53",
+    "url": "/static/media/button.3166ed04.svg"
+  },
+  {
+    "revision": "d055a67b4af74360d394caf32dc6b9ef",
+    "url": "/static/media/save-btn.d055a67b.svg"
+  },
+  {
+    "revision": "b1446a1c657ab9c13920c5bdab5769ca",
+    "url": "/index.html"
+  }
+];
